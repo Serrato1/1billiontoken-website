6522614af827e99c18931a63c6c63c8f072b8652
Alvin J. Alexander <serrato.dev@gmail.com>
2021-05-25 01:50:59 -0700

    added accurate countdown timer

--- src/sections/CoinFund/index.js
+++ src/sections/CoinFund/index.js
@@ -19,7 +19,7 @@ import CoinFundWrapper from "./coinFund.style";
 
 const CoinFund = () => {
   const settings = { 
-    count: 36000,
+    count: 36600,
     showTitle: true,
     size: 60,
     labelSize: 14,
