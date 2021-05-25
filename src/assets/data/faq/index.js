dbfbf0bb887998e121ca9b1f15cc5fd25a62e1e8
Alvin J. Alexander <serrato.dev@gmail.com>
2021-05-25 16:50:35 -0700

    added tutorial for buy now

--- src/assets/data/faq/index.js
+++ src/assets/data/faq/index.js
@@ -10,7 +10,20 @@ export default {
             },
             {
                 title: "How can I purchase?",
-                body: "To purchase our coin, follow this link"
+                body: `
+                    1: Download Trust Wallet
+                    2: Buy BNB
+                    3. Convert to Smart Chain BNB 
+                    3: Copy & paste this >>> into safari Trust://browser_enable
+                    4: in safari allow it to open in trust wallet
+                    5: hit browser
+                    6: click pancake swap to open
+                    7: make sure you’re under swap and that your slippage is at 30%
+                    8: under the “from” make sure the currency is “BNB”
+                    9: under the “to” copy and paste the contract “ 0x34d74a40b32def4c428e567608c36ed774e801c1 “ to buy 
+                    10: hit MAX under the “from” section 
+                    11: finish and swap from BNB to 1BILL
+                `
             },
             {
                 title: "Is this is a Scam?",
